570556aeb02464e8d47062dc8914bc0b62eaf1a0
Dewayne VanHoozer <dvanhoozer@gmail.com>
2024-09-21 21:45:36 -0500

    split-on-sentences
    This new sub-class will chunk at the sentence level where a sentence is defined as one or more characters that precede a period, question mark or a banger which is follow by one of more white-space characters.

--- lib/baran.rb
+++ lib/baran.rb
@@ -5,6 +5,7 @@ require_relative "baran/text_splitter"
 require_relative "baran/markdown_splitter"
 require_relative "baran/recursive_character_text_splitter"
 require_relative "baran/character_text_splitter"
+require_relative "baran/sentence_text_splitter"
 
 module Baran
   class Error < StandardError; end
